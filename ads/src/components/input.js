e3bd9cd7c949675da432b5ee2bc77d2ceffee86f
Vighnesh Mishra <vighnesh97531@gmail.com>
2023-12-17 02:41:49 +0530

    Minor

--- ads/src/components/input.js
+++ ads/src/components/input.js
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
-import axios from 'axios'; // For making HTTP requests
+import axios from 'axios';
 
 const App = () => {
-  const [companyName, setCompanyName] = useState('');
-  const [productInfo, setProductInfo] = useState('');
-  const [theme, setTheme] = useState('');
-  const [adType, setAdType] = useState('');
-  const [tagline, setTagline] = useState('');
+  const [formData, setFormData] = useState({
+    companyName: '',
+    productInfo: '',
+    theme: '',
+    adType: '',
+    tagline: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    // Prepare the data to send to the backend
-    const data = {
-      companyName,
-      productInfo,
-      theme,
-      adType,
-      tagline
-    };
 
-    // Send POST request to Flask backend
     try {
-      const response = await axios.post('http://127.0.0.1:5000/save-data', data);
+      const { companyName, productInfo, theme, adType, tagline } = formData;
+      const slug = `/${companyName}/${productInfo}/${theme}/${adType}/${tagline}`; 
+
+      const response = await axios.post(`http://127.0.0.1:5000/save-data${slug}`);
       console.log('Data submitted:', response.data);
-      // Reset the form after successful submission
-      setCompanyName('');
-      setProductInfo('');
-      setTheme('');
-      setAdType('');
-      setTagline('');
+      setFormData({
+        companyName: '',
+        productInfo: '',
+        theme: '',
+        adType: '',
+        tagline: ''
+      });
     } catch (error) {
       console.error('Error submitting data:', error);
     }
@@ -40,19 +41,19 @@ const App = () => {
       <h1>Ad Information Form</h1>
       <form onSubmit={handleSubmit}>
         <label>Company Name:</label>
-        <input type="text" value={companyName} onChange={(e) => setCompanyName(e.target.value)} required />
+        <input type="text" name="companyName" value={formData.companyName} onChange={handleChange} required />
         <br />
         <label>Product Information:</label>
-        <input type="text" value={productInfo} onChange={(e) => setProductInfo(e.target.value)} required />
+        <input type="text" name="productInfo" value={formData.productInfo} onChange={handleChange} required />
         <br />
         <label>Theme:</label>
-        <input type="text" value={theme} onChange={(e) => setTheme(e.target.value)} required />
+        <input type="text" name="theme" value={formData.theme} onChange={handleChange} required />
         <br />
-        <label>Type of Ad:</label>
-        <input type="text" value={adType} onChange={(e) => setAdType(e.target.value)} required />
+        <label>Ad Type:</label>
+        <input type="text" name="adType" value={formData.adType} onChange={handleChange} required />
         <br />
         <label>Tagline (optional):</label>
-        <input type="text" value={tagline} onChange={(e) => setTagline(e.target.value)} />
+        <input type="text" name="tagline" value={formData.tagline} onChange={handleChange} />
         <br />
         <button type="submit">Submit</button>
       </form>
